f55642e91fa7d7c0415c6a4289ae55835ba71b10
Amit Roushan <amit.roushan@huawei.com>
2022-05-20 08:42:26 +0530

    meta service framework

---
+++ test/e2e/meta_service/fake_backuplocation.go
@@ -0,0 +1,137 @@
+// Copyright 2022 The SODA Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package meta_service
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	pb "github.com/soda-cdm/kahu/provider/lib/go"
+)
+
+type FakeNFSDriver struct {
+	unixSocketPath string
+	dumpYard       string
+	server         *grpc.Server
+}
+
+func NewFakeNFSDriver(unixSocketPath string, dumpYard string) *FakeNFSDriver {
+	return &FakeNFSDriver{
+		unixSocketPath: unixSocketPath,
+		dumpYard:       dumpYard,
+	}
+}
+
+func (f *FakeNFSDriver) RunDriver() error {
+	log.Info("Starting fake NFS Server ...")
+
+	lis, err := net.Listen("unix", f.unixSocketPath)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+	f.server = grpc.NewServer(opts...)
+	pb.RegisterMetaBackupServer(f.server, f)
+
+	return f.server.Serve(lis)
+}
+
+func (f *FakeNFSDriver) StopDriver() error {
+	log.Info("Stopping fake NFS Server ...")
+	f.server.Stop()
+	return nil
+}
+
+func (f *FakeNFSDriver) Upload(upload pb.MetaBackup_UploadServer) error {
+	log.Info("Upload Called .... ")
+
+	uploadRequest, err := upload.Recv()
+	if err != nil {
+		return status.Errorf(codes.Unknown, "failed with error %s", err)
+	}
+
+	fileInfo := uploadRequest.GetInfo()
+	if fileInfo == nil {
+		return status.Errorf(codes.InvalidArgument, "first request is not file info")
+	}
+
+	fileName := fileInfo.GetFileIdentifier()
+	// TODO: support file attributes
+
+	// create buffer t o store data
+	fileData := bytes.Buffer{}
+
+	for {
+		uploadRequest, err = upload.Recv()
+		// If there are no more requests
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Errorf("uploadRequest received error %s", err)
+			return status.Errorf(codes.Unknown, "error receiving request")
+		}
+
+		data := uploadRequest.GetChunkData()
+		_, err = fileData.Write(data)
+		if err != nil {
+			log.Errorf("error writing request %s", err)
+			return status.Errorf(codes.Unknown, "error writing request %s", err)
+		}
+	}
+
+	file, err := os.Create(filepath.Join(f.dumpYard, fileName))
+	if err != nil {
+		log.Errorf("file create error %s", err)
+		return status.Errorf(codes.Unknown, "file create error %s", err)
+	}
+
+	_, err = fileData.WriteTo(file)
+	if err != nil {
+		log.Errorf("uploadRequest received error %s", err)
+		return status.Errorf(codes.Unknown, "error receiving request %s", err)
+	}
+
+	err = upload.SendAndClose(&pb.Empty{})
+	if err != nil {
+		return status.Errorf(codes.Unknown, "failed to close and flush file. %s", err)
+	}
+
+	return nil
+}
+
+func (f *FakeNFSDriver) ObjectExists(context.Context,
+	*pb.ObjectExistsRequest) (*pb.ObjectExistsResponse, error) {
+	return nil, fmt.Errorf("Not Implemented")
+}
+
+func (f *FakeNFSDriver) Download(*pb.DownloadRequest, pb.MetaBackup_DownloadServer) error {
+	return fmt.Errorf("Not Implemented")
+}
+
+func (f *FakeNFSDriver) Delete(context.Context, *pb.DeleteRequest) (*pb.Empty, error) {
+	return nil, fmt.Errorf("Not Implemented")
+}
---
+++ test/e2e/meta_service/meta_service.go
@@ -0,0 +1,126 @@
+// Copyright 2022 The SODA Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package meta_service
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"google.golang.org/grpc"
+
+	repo "github.com/soda-cdm/kahu/providerframework/meta_service/backup_respository"
+	"github.com/soda-cdm/kahu/providerframework/meta_service/cmd/options"
+	pb "github.com/soda-cdm/kahu/providerframework/meta_service/lib/go"
+	"github.com/soda-cdm/kahu/providerframework/meta_service/server"
+)
+
+var _ = Describe("MetaService", func() {
+	var (
+		address          = "127.0.0.1"
+		port             = 8181
+		grpcServer       *grpc.Server
+		grpcConnection   *grpc.ClientConn
+		metaClient       pb.MetaServiceClient
+		driver           *FakeNFSDriver
+		driverSocketPath string
+		driverDumpYard   string
+	)
+
+	BeforeEach(func() {
+		// init driver
+		currentDir, err := os.Getwd()
+		Expect(err).To(BeNil())
+		driverSocketPath = filepath.Join(currentDir, "nfs_driver")
+		driverDumpYard = currentDir
+		driver = NewFakeNFSDriver(driverSocketPath, driverDumpYard)
+		go driver.RunDriver()
+
+		// default config
+		config := options.NewMetaServiceFlags()
+
+		// initialize backup repository
+		repository, err := repo.NewBackupRepository(driverSocketPath)
+		Expect(err).To(BeNil())
+
+		// initialize server
+		lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d",
+			address, port))
+		Expect(err).To(BeNil())
+
+		var serverOpts []grpc.ServerOption
+		grpcServer = grpc.NewServer(serverOpts...)
+		pb.RegisterMetaServiceServer(grpcServer,
+			server.NewMetaServiceServer(context.Background(), options.MetaServiceOptions{
+				MetaServiceFlags: *config,
+			}, repository))
+		go grpcServer.Serve(lis)
+
+		// initialize client
+		grpcConnection, err = pb.NewLBDial(fmt.Sprintf("%s:%d", address, port),
+			grpc.WithInsecure())
+		Expect(err).To(BeNil())
+
+		metaClient = pb.NewMetaServiceClient(grpcConnection)
+	})
+
+	AfterEach(func() {
+		grpcServer.Stop()
+		err := grpcConnection.Close()
+		Expect(err).To(BeNil())
+
+		err = driver.StopDriver()
+		Expect(err).To(BeNil())
+	})
+
+	It("Ensure basic Flow", func() {
+		backupClient, err := metaClient.Backup(context.Background())
+		Expect(err).To(BeNil())
+
+		// send backup info
+		err = backupClient.Send(&pb.BackupRequest{
+			Backup: &pb.BackupRequest_Identifier{
+				Identifier: &pb.BackupIdentifier{
+					BackupHandle: "test_basic_backup",
+				},
+			},
+		})
+		Expect(err).To(BeNil())
+
+		// send backup data
+		err = backupClient.Send(&pb.BackupRequest{
+			Backup: &pb.BackupRequest_BackupResource{
+				BackupResource: &pb.BackResource{
+					Resource: &pb.Resource{
+						Name:    "test",
+						Group:   "test_group",
+						Version: "v1beta1",
+						Kind:    "Test",
+					},
+					Data: []byte("test it ....... "),
+				},
+			},
+		})
+		Expect(err).To(BeNil())
+
+		// Close stream
+		_, err = backupClient.CloseAndRecv()
+		Expect(err).To(BeNil())
+	})
+})
